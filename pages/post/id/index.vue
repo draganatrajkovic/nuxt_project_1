7f4070472877104155b047b956198022b3024871
Dragana <distant.countries@gmail.com>
2020-05-26 15:07:10 +0200

    Commit3 sass implementation + nuxt-links practice

---
+++ pages/post/id/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        id: {{$route.params.id}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                
+            }
+        }
+    }
+</script>
