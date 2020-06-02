3b1b78228a40c13983d06f202d989e972ac5c015
Dragana Trajkovic <dragana.trajkovic.it@gmail.com>
2020-06-03 01:34:49 +0200

    Custom slider functionality done

--- components/blocks/BlockOurStory.vue
+++ components/blocks/BlockOurStory.vue
@@ -6,12 +6,7 @@
                 <div class="img img--center block-our-story__img">
                     <img src="~/static/BlockOurStory/bg.png" alt="our_story_img"/>
                 </div>
-                <VueSlickCarousel 
-                    :arrows="false" 
-                    :dots="true" 
-                    ref="banner"
-                    class="slider--white"
-                >
+                <VueSlickCarousel :arrows="false" :dots="false" ref="banner"class="slider--white">
                     <div>
                         <h2 class="title title--white block-our-story__subtitle text--center">Doing the Bright Thing Since 1979</h2>
                         <p class="text text--white text--center">It all began with a thermistor disk. The year was 1979 and, at 
@@ -35,11 +30,14 @@
                             look like readable English.</p>
                     </div>
                 </VueSlickCarousel>
-            </div>
-
-            <div class="">
-                <a href="javascript:;" @click="slickNext">Next</a>
-                <a href="javascript:;" @click="slickPrev">Prev</a>
+                <div class="slider__arrows box__section box__section--inline">
+                    <a href="javascript:;" @click="slickPrev" class=" slider__arrow slider__arrow-left"></a>
+                    <div v-for="(dot, index) in 3" :key="index">
+                        <div v-if="selected===(index+1)" class="slider_dot"><img src="./../../static/PartsSlider/dot--white_selected.png" /></div>
+                        <div v-else class="slider_dot"><img src="./../../static/PartsSlider/dot.png" /></div>
+                    </div>
+                    <a href="javascript:;" @click="slickNext" class="slider__arrow slider__arrow-right"></a>
+                </div>
             </div>
         </div>
     </div>
@@ -48,18 +46,34 @@
 <script>
 import VueSlickCarousel from 'vue-slick-carousel'
 import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-// optional style for arrows & dots
 import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 export default {
     components: {
         VueSlickCarousel
     },
+    data() {
+        return {
+            selected: 1
+        }
+    },
     methods: {
         slickNext() {
+            if (this.selected === 3) {
+                this.selected = 1
+            } else {
+                this.selected += 1
+            }
             this.$refs.banner.next();
         },
         slickPrev() {
             this.$refs.banner.prev();
+            if (this.selected === 0) {
+                this.selected = 3
+            } if (this.selected === 1) {
+                this.selected = 3
+            } else {
+                this.selected -= 1
+            }
         }
     },
 }
@@ -78,6 +92,38 @@ export default {
     }
     .block-our-story__subtitle {
         margin-bottom: 25px;
+    }
+    .slider__arrows {
+        justify-content: center;
+        align-items: center;
+    }
+    .slider__arrow {
+        width: 71px;
+        height: 71px;
+        &:hover {
+            cursor: pointer;
+            transition: 0.3s;
+        }
+    }
+    .slider__arrow-left {
+        background: url('./../../static/PartsSlider/arrow_left--white.png') no-repeat center center;
+        &:hover {
+            background: url('./../../static/PartsSlider/arrow_left--white_hover.png') no-repeat center center;
+        }
+    }
+    .slider__arrow-right {
+        background: url('./../../static/PartsSlider/arrow_right--white.png') no-repeat center center;
+        &:hover {
+            background: url('./../../static/PartsSlider/arrow_right--white_hover.png') no-repeat center center;
+        }
+    }
+    .slider_dot {
+        width: 7px;
+        height: 6px;
+        margin: 0 5px;
+        display: flex;
+        justify-content: center;
+        align-content: center;
     }
 
 </style>
